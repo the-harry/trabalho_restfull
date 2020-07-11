3ac51aa6deca22aaa13fbe39aa420cb41bcc17ad
Matheus Acosta <matheusthebr@gmail.com>
2020-07-11 18:04:55 -0300

    Load alunos list from json file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+sala = Sala.create(nome: '2TIN')
+
+JSON.parse(File.read('db/alunos.json'))['alunos'].each do |aluno|
+  Aluno.create(nome: aluno['nome'], rm: aluno['rm'], sala: sala)
+end
