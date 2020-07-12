e5f3b1896aa849d31bcafe5b5f911f8065285007
Matheus Acosta <matheusthebr@gmail.com>
2020-07-12 18:44:33 -0300

    Created trabalhos controller create action.

--- app/controllers/api/v1/trabalhos_controller.rb
+++ app/controllers/api/v1/trabalhos_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::TrabalhosController < Api::V1::ApiController
   def index; end
 
-  def create; end
+  def create
+    trabalho = Trabalho.new(sanitized_trabalho(params))
+
+    if trabalho.save
+      render status: :created, json: { trabalho_id: trabalho.id }
+    else
+      render status: :unprocessable_entity, json: { erros: trabalho.errors }
+    end
+  end
 
   def show; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def sanitized_trabalho(params)
+    params.require(:trabalho).permit(:title, :url, :aluno_id)
+  end
 end
---
+++ spec/requests/create_trabalho_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe 'Aluno cria novo trabalho', type: :request do
+  let(:aluno) { Aluno.create(nome: 'foo', rm: 123, sala: Sala.create(nome: 'bar')) }
+
+  context 'success 201' do
+    it 'responde com o id do trabalho' do
+      post "/api/v1/trabalhos", params: { trabalho: { title: 'NAC', url: 'localhost', aluno_id: aluno.id } }
+
+      expect(response).to have_http_status(:created)
+      expect(response.body).to eq('{"trabalho_id":1}')
+    end
+  end
+
+  context 'failure 422' do
+    it "raises error if no title is suplied" do
+      post "/api/v1/trabalhos", params: { trabalho: { url: 'localhost', aluno_id: aluno.id } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "raises error if no url is suplied" do
+      post "/api/v1/trabalhos", params: { trabalho: { title: 'NAC', aluno_id: aluno.id } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "raises error if no student is suplied" do
+      post "/api/v1/trabalhos", params: { trabalho: { title: 'NAC', url: 'localhost' } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
