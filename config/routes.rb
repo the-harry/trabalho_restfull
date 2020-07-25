c0922f2541fa2dd95665803af72f573379701893
Matheus Acosta <matheusthebr@gmail.com>
2020-07-25 14:57:02 -0300

    Fix rest resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :trabalhos, only: %i[index create show update destroy]
+      resources :trabalhos, only: %i[index]
+      resources :trabalho, only: %i[create show update destroy], controller: :trabalhos
     end
   end
 end
