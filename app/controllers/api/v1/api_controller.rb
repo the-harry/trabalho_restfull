336081d5e8cea3e9767e78e8dd5e0796c30a94be
Matheus Acosta <matheusthebr@gmail.com>
2020-07-26 11:53:41 -0300

    Deal with errors in API controller.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class Api::V1::ApiController < ApplicationController
+  rescue_from StandardError do |erro|
+    if erro.is_a?(ActiveRecord::RecordNotFound)
+      response[:status] = 404
+      response[:body] = { error: :not_found, message: 'Registro nao encontrado.' }
+    end
+
+    if erro.is_a?(ActiveRecord::RecordInvalid)
+      response[:status] = 422
+      response[:body] = { error: :unprocessable_entity, message: erro.record.errors }
+    end
+
+    if erro.is_a?(ActionController::ParameterMissing)
+      response[:status] = 422
+      response[:body] = { error: :unprocessable_entity, message: 'Missing params.' }
+    end
+
+    response[:status] ||= 500
+    response[:body] ||= { error: :internal_server_error, message: erro.to_s }
+
+    render status: response[:status], json: response[:body]
+  end
 end
