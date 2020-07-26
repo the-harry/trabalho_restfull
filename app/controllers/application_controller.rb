ccab37ea7c1f00684be063bc9c44f7190599eb70
Matheus Acosta <matheusthebr@gmail.com>
2020-07-26 14:47:43 -0300

    Added auth methods to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 end
